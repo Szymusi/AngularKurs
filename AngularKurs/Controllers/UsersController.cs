98560fd9737619fb9c675be436d3aedeb9e671fa
sdanielak <sdanielak@DST-DELL-006-1>
2021-08-16 12:41:13 +0200

    Add project files.

---
+++ AngularKurs/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using AngularKurs.Data;
+using AngularKurs.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularKurs.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = _context.Users.ToListAsync();
+            return await users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = _context.Users.SingleOrDefaultAsync(x => x.Id == id);
+            return await user;
+        }
+    }
+}
